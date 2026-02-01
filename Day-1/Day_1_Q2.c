fb44768a57e2b5d7695bcea390fabddd18d3a31b
vishishta-b <vishishtabiswas@gmail.com>
2026-02-01 20:49:00 +0530

    Day 1 - DSA practice

---
+++ Day-1/Day_1_Q2.c
@@ -0,0 +1,30 @@
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    *returnSize = 2;                              
+    int *result = (int *)malloc(2 * sizeof(int)); 
+
+    
+    for (int i = 0; i < numsSize; i++)
+    {
+        
+        for (int j = i + 1; j < numsSize; j++)
+        {
+
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+
+    return 0;
+}
