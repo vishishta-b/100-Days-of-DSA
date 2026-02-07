00c3445f962555fe2ec01bf505b47ec265400d5f
vishishta-b <vishishtabiswas@gmail.com>
2026-02-07 15:40:00 +0530

    Day 7 - DSA practice

---
+++ Day-7/Day_7_Q1.c
@@ -0,0 +1,23 @@
+
+
+
+
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    if (scanf("%d", &n) != 1)
+        return 1;
+    printf("%d\n", fib(n));
+    return 0;
+}
