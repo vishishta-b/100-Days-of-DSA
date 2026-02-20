e9403b46bd7fe1c41fe13ac32974c2f4c6a91860
vishishta-b <vishishtabiswas@gmail.com>
2026-02-20 11:45:00 +0530

    Day 20 - DSA practice

---
+++ Day-20/Day_20_Q1.c
@@ -0,0 +1,29 @@
+
+
+
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum == 0)
+            {
+                count++;
+            }
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
