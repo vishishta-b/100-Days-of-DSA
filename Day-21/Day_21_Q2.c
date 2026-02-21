525aa5b54e970aed1dd5ffe7680a24fba5c18e94
vishishta-b <vishishtabiswas@gmail.com>
2026-02-21 14:28:00 +0530

    Day 21 - DSA practice

---
+++ Day-21/Day_21_Q2.c
@@ -0,0 +1,19 @@
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode *middleNode(struct ListNode *head)
+{
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
