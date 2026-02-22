01b0bcb7eccccb720d519e38220bb298baa2a94e
Vishishta Biswas <77668338+vishishta-b@users.noreply.github.com>
2026-02-22 23:03:07 +0530

    Add linked list implementation in Q1.c
    Implement a linked list to store and print integers.

---
+++ Day21/Q1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *tail = NULL;
+
+    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        if (temp->next != NULL) {
+            printf(" ");
+        }
+        temp = temp->next;
+    }
+    printf("\n");
+
+    temp = head;
+    while (temp != NULL) {
+        struct Node* nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
+
+    return 0;
+}
