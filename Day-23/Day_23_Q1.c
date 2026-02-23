06c04834eaf3565054eda3f756cb5fb6ad9cef95
vishishta-b <vishishtabiswas@gmail.com>
2026-02-23 14:01:00 +0530

    Day 23 - DSA practice

---
+++ Day-23/Day_23_Q1.c
@@ -0,0 +1,52 @@
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
+{
+    if (list1 == NULL)
+        return list2;
+    if (list2 == NULL)
+        return list1;
+    struct ListNode *head = NULL;
+    struct ListNode *tail = NULL;
+    if (list1->val <= list2->val)
+    {
+        head = list1;
+        list1 = list1->next;
+    }
+    else
+    {
+        head = list2;
+        list2 = list2->next;
+    }
+    tail = head;
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->val <= list2->val)
+        {
+            tail->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+    if (list1 != NULL)
+        tail->next = list1;
+    if (list2 != NULL)
+        tail->next = list2;
+    return head;
+}
