908593469de39fd12b57e15fd26dde937fb882ed
vishishta-b <vishishtabiswas@gmail.com>
2026-03-22 20:56:00 +0530

    Day 50 - DSA practice

---
+++ Day-50/Day_50_Q2.c
@@ -0,0 +1,29 @@
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode *searchBST(struct TreeNode *root, int val)
+{
+    if (root == NULL || root->val == val)
+    {
+        return root;
+    }
+
+    if (val < root->val)
+    {
+        return searchBST(root->left, val);
+    }
+
+    return searchBST(root->right, val);
+}
